f2bb07f090298072f768f9c25775130deeeb8338
Kevin Smith <keveightysev@gmail.com>
2019-02-26 21:48:31 -0600

    Added rotation functionality to Gingham pattern

--- src/components/Gingham/Gingham.js
+++ src/components/Gingham/Gingham.js
@@ -7,9 +7,10 @@ class Gingham extends React.Component {
     constructor() {
         super();
         this.state = {
-            degrees: 45,
+            angle: 45,
             colors: ['#FFFFFF', '#006708'],
             width: 50,
+            height: 50,
         }
     }
 
@@ -30,9 +31,22 @@ class Gingham extends React.Component {
     newCSS = () => {
         const rgbColor = this.hexToRgb(this.state.colors[1]);
 
-        return `background-color:${this.state.colors[0]};
+        return `position: relative;overflow: hidden;
+        width: 100%;
+        height: 100%;
+        &:before {
+            content: '';
+    position: absolute;
+    width: 200%;
+    height: 200%;
+    top: -50%;
+    left: -50%
+    z-index: -100;
+            background-color:${this.state.colors[0]};
 background-image: linear-gradient(90deg, rgba(${rgbColor.r},${rgbColor.g},${rgbColor.b},.6) 50%, transparent 50%),linear-gradient(rgba(${rgbColor.r},${rgbColor.g},${rgbColor.b},.4) 50%, transparent 50%);
-background-size:${this.state.width}px ${this.state.width}px;`;
+background-size:${this.state.width}px ${this.state.height}px;
+transform: rotate(${this.state.angle}deg);
+        }`;
     }
 
     
@@ -52,14 +66,45 @@ background-size:${this.state.width}px ${this.state.width}px;`;
         const css = this.newCSS();
         this.props.updateCSS(css);
     }
+
+    handleAngleChange = e => {
+        this.setState({
+            angle: e.target.value,
+        })
+        const css = this.newCSS();
+        this.props.updateCSS(css);
+    }
     
     render() {
-        const css = this.newCSS();
+        const rgbColor = this.hexToRgb(this.state.colors[1]);
+        const css = `div {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+}
+
+div:before {
+    content: '';
+    position: absolute;
+    width: 200%;
+    height: 200%;
+    top: -50%;
+    left: -50%
+    z-index: -100;
+    background-color:${this.state.colors[0]};
+    background-image: 
+        linear-gradient(90deg, rgba(${rgbColor.r},${rgbColor.g},${rgbColor.b},.6) 50%, transparent 50%),
+        linear-gradient(rgba(${rgbColor.r},${rgbColor.g},${rgbColor.b},.4) 50%, transparent 50%);
+    background-size:${this.state.width}px ${this.state.height}px;
+    transform: rotate(${this.state.angle}deg);
+}`
         return (
             <Box>
                 <GinghamForm {...this.state} 
                     handleColorChange={this.handleColorChange}
                     handleSlider={this.handleSlider}
+                    handleAngleChange={this.handleAngleChange}
                     updateCSS={this.props.updateCSS}
                     />
                 <CSSBox css={css} />
--- src/components/Gingham/GinghamForm.js
+++ src/components/Gingham/GinghamForm.js
@@ -7,10 +7,11 @@ const GinghamForm = props => {
   return (
     <Form>
       {props.colors.map((color, index) => <ColorPicker color={color} index={index} handleColorChange={props.handleColorChange} key={index} />)}
-      <h2>Select a Width</h2>
       <div>
+        <h2>Scale the Pattern</h2>
         <input type="range" min="10" max="999" value={props.width} onChange={props.handleSlider} />
-
+        <h2>Select an Angle</h2>
+        <input type="range" min="0" max="180" value={props.angle} onChange={props.handleAngleChange} />
       </div>
     </Form>
   );
